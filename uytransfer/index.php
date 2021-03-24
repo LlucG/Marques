442233a4cd92f15e0c0f5666dff909a2ba93e1ed
Lluc <lluc.gimenez@mataro.epiaedu.cat>
2021-03-24 20:31:11 +0100

    Afegit JavaScript, canvi Upload, acabat exercici 6 menys error correu amb get

--- uytransfer/index.php
+++ uytransfer/index.php
@@ -8,23 +8,23 @@
 </head>
 
 <body>
-	<?php include 'php/header.php'; ?>
+	<?php include 'php/header.php';?>
 	<div id="caixa">
 		<form action="upload.php" method="post" enctype="multipart/form-data">
 			<ul>
 				<input type="text" name="nom" placeholder="El teu nom" class="input">
 			</ul>
 			<ul>
-				<input type="file" name="arxiu" placeholder="Selecciona el teu arxiu" class="input">
+				<input type="file" name="arxiu" accept=".pdf,.png,.jpg,.rar,.zip" placeholder="Selecciona el teu arxiu" class="input">
 			</ul>
 			<ul>
 				<input type="checkbox" name="linksino" id="linksino">
 				<label for="linksino"><strong>Vols envair el link per correu?</strong></label>
 			</ul>
 			<ul>
-				<input type="text" name="email" placeholder="Email destinatari" class="input">
+				<input type="text" name="email" id="email" placeholder="Email destinatari" class="input d-none">
 			</ul>
-			<ul>
+			<ul class="d-none" id="missatge">
 				<label><strong>Missatge</strong></label><br>
 				<textarea name="missatge" class="input" rows="6"></textarea>
 			</ul>
@@ -33,5 +33,16 @@
 			</ul>
 		</form>
 	</div>
+	<script type="text/javascript">
+		document.getElementById("linksino").addEventListener('change', function () {
+			if (this.checked) {
+    			document.getElementById("email").classList.remove("d-none");
+    			document.getElementById("missatge").classList.remove("d-none");
+  			} else {
+    			document.getElementById("email").classList.add("d-none");
+    			document.getElementById("missatge").classList.add("d-none");
+  			}
+		});
+	</script>
 </body>
 </html>
--- uytransfer/upload.php
+++ uytransfer/upload.php
@@ -10,17 +10,47 @@
 <body>
 	<?php include 'php/header.php'; ?>
 	<?php
-		//print_r($_POST);
-		//print_r($_FILES);
-		$imgerror = false;
-		$nom = date("Y") . date("m") . date("d") . rand(11111, 99999);
+		#print_r($_POST);
+		#print_r($_FILES);
+		$extensiobool = false;
+		$midabool = false;
 		$extensio = pathinfo($_FILES['arxiu']['name'], PATHINFO_EXTENSION);
-		move_uploaded_file($_FILES['arxiu']['tmp_name'], "files/". $nom . "." .$extensio);
-		$link ='<a href="http://localhost/Marques/uytransfer/files/'.$nom.".".$extensio.'"><p id="textUpload" style="margin-bottom: 2em;">http://localhost/Marques/uytransfer/files/'.$nom.'</p></a>';
+		if ($extensio == 'pdf' || $extensio == 'png' || $extensio == 'jpg' || $extensio == 'rar' || $extensio == 'zip') {
+			$extensiobool = true;
+		}
+		if ($_FILES['arxiu']['size'] < 10000) {
+			$midabool = true;
+		}
+
+		if ($extensiobool && $midabool) {
+			$nom = date("Y") . date("m") . date("d") . rand(11111, 99999);
+			move_uploaded_file($_FILES['arxiu']['tmp_name'], "files/". $nom . "." .$extensio);
+			$link ='http://localhost/Marques/uytransfer/files/'.$nom.'.'.$extensio;
+		}
+
+		//Enviar mail php
+		/*Abans d'enviar el correu electrònic es verificarà que l'email introduït tingui format d'email, és a dir, que contingui el caràcter @. Si no és així, es redireccionarà a l'usuari a index.php informant de l'error. Aquest error serà enviat com a paràmetre d'una petició get (per exemple, index.php?error_mail=1). Al mostrar novament el formulari de pujada (index.php) l'usuari ha de visualitzar algun missatge d'error que informi que l'email indicat no és vàlid.*/
+		$email = $_POST["email"];
+		$emailcorrectebool = false;
+		if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+			$emailcorrectebool = true;
+		} else {
+			header('Location: index.php');
+		}
+
+		if ($emailcorrectebool && $extensiobool && $midabool && $_POST["linksino"] = 'on') {
+			$tema = 'Link Uy Transfer';
+			if (empty($_POST["missatge"])) {
+				$missatge = 'Sorpresa!! Algú ha compartit un arxiu amb tu.' . "\n" . $link;
+			} else {
+				$missatge = $_POST["missatge"] . "\n" . $link;
+			}
+			mail($email, $tema, $missatge);
+		}
 	?>
 	<div id="caixaUpload">
 		<?php 
-			if ($imgerror) {
+			if (empty($_FILES['arxiu']['name']) || !$extensiobool || !$midabool) {
 				echo '<img src="images/error.jpg" style="border-radius: 50%; float: left;">';
 			} else {
 				echo '<img src="images/correcte.jpg" style="border-radius: 50%; float: left;">';
@@ -28,20 +58,39 @@
 		?>
 		<table id="table">
 			<tr>
-				<td><p id="titolUpload">Arxiu enviat correctament</p></td>
+				<td>
+					<?php 
+						if (!empty($_FILES['arxiu']['name']) && $extensiobool && $midabool) {
+							echo '<p id="titolUpload" style="margin-left: 2em;">Arxiu enviat correctament</p>';
+						} else if (!empty($_FILES['arxiu']['name']) && !$extensiobool) {
+							echo '<p id="titolUpload" style="margin-top: 2.5em; margin-bottom: 2.5em; margin-left: 3em;">El format no és compatible</p></td>';
+						} else if (!empty($_FILES['arxiu']['name']) && !$midabool) {
+							echo '<p id="titolUpload" style="margin-top: 2.5em; margin-bottom: 2.5em; margin-left: 3em;">L\'arxiu és massa gran</p></td>';
+						} else {
+							echo '<p id="titolUpload" style="margin-top: 2.5em; margin-bottom: 2.5em; margin-left: 3em;">No \'ha enviat cap arxiu</p></td>';
+						}
+					?>
 			</tr>
 			<tr>
 				<td>
-					<?php if (!empty($_POST["nom"])) {
-						echo '<p id="textUpload" style="margin-top: 0em;">Hola ',$_POST["nom"],' utilitza aquest link per compartir l\'arxiu</p>';
-					} else {
-						echo '<p id="textUpload" style="margin-top: 0em;">Oye tu utilitza aquest link per compartir l\'arxiu</p>';
-					} ?>
-
+					<?php
+						if (!empty($_FILES['arxiu']['name']) && $extensiobool && $midabool) {
+							if (!empty($_POST["nom"])) {
+								echo '<p id="textUpload" style="margin-top: 0em; margin-left: 4em;">Hola ',$_POST["nom"],' utilitza aquest link per compartir l\'arxiu</p>';
+							} else {
+								echo '<p id="textUpload" style="margin-top: 0em; margin-left: 5em;">Oye tu utilitza aquest link per compartir l\'arxiu</p>';
+							} 
+						}
+					?>
 				</td>
 			</tr>
 			<tr>
-				<td><?php echo $link ?></td>
+				<td><?php
+						if (!empty($_FILES['arxiu']['name']) && $extensiobool && $midabool) {
+							echo '<a href="'.$link.'"><p id="textUpload" style="margin-bottom: 2em; margin-left: 3.2em;">http://localhost/Marques/uytransfer/files/'.$nom.'</p></a>';
+						}
+					?>
+				</td>
 			</tr>
 		</table>
 		
