82d9c1a128b7c366f216ecd43b25ef119f305d1e
Lluc <lluc.gimenez@mataro.epiaedu.cat>
2021-04-15 17:35:21 +0200

    A mitjes exercici 3

--- supermarket/form_client.php
+++ supermarket/form_client.php
@@ -67,7 +67,6 @@
 									} else {
 										echo "ERROR al seleccionar les dades";
 									}
-									$conn->close();
 								?>
 							</select>
 						</div>
@@ -85,6 +84,51 @@
 					</div>
 				</div>
 			</form>
+			<?php
+				if (!empty($_POST["username"])) {$username = $_POST["username"];}
+				if (!empty($_POST["username"])) {$pass = $_POST["pass"];}
+				if (!empty($_POST["rp_pass"])) {$rp_pass = $_POST["rp_pass"];}
+				if (!empty($_POST["nombre"])) {$nombre = $_POST["nombre"];}
+				if (!empty($_POST["apellidos"])) {$apellidos = $_POST["apellidos"];}
+				if (!empty($_POST["nif"])) {$nif = $_POST["nif"];}
+				if (!empty($_POST["direccion"])) {$direccion = $_POST["direccion"];}
+				if (!empty($_POST["codigo_postal"])) {$codigo_postal = $_POST["codigo_postal"];}
+				if (!empty($_POST["poblacion"])) {$poblacion = $_POST["poblacion"];}
+				if (!empty($_POST["telefono"])) {$telefono = $_POST["telefono"];}
+				if (!empty($_POST["mail"])) {$mail = $_POST["mail"];}
+
+				if (isset($username) && isset($pass) && isset($nombre) && isset($apellidos) && isset($nif) && isset($direccion) && isset($codigo_postal) && isset($poblacion)) {
+					$usuari = $conn->query("SELECT nom_usuari FROM clients WHERE nom_usuari = $username");
+					if (empty($usuari)) {
+						if ($pass = $rp_pass) {
+							if (strlen($pass) > 3) {
+								$nif_len = strlen($nif);
+								if ($nif_len = 9 && is_numeric(substr($nif, 0, 8)) && !is_numeric($nif[9])) {
+									if (str_contains($mail, '@') && str_contains($mail, '.')) {
+										$sql = "INSERT INTO clients (nom_usuari, contrasenya, nom, cognoms, nif, adreca, codigo_postal, poblacio, telefon, email) VALUES ($username, $pass, $nombre, $apellidos, $nif, $direccion, $codigo_postal, $poblacion, $telefono, $mail)";
+										//print_r($sql);
+										$conn->query($sql);
+									} else {
+										echo "Email incorrecte";
+									}
+								} else {
+									echo "Nif incorrecte";
+								}
+							} else {
+								echo "La contrasenya és massa curta";
+							}
+						} else {
+							echo "Les contrasenyes no coincideixen";
+						}
+					} else {
+						echo "Aquest usuari ja existeix";
+					}
+				} else {
+					echo "Falten dades obligatories";
+				}
+
+				$conn->close();
+			?>
 		</div>
 	</body>
 </html>
